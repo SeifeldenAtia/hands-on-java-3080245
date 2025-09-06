e239986c1a243f4f7b023d4614b59118df39603c
Seifelden Atia <68141338+SeifeldenAtia@users.noreply.github.com>
2025-09-06 15:48:18 +0000

    getting a customer from the database with getCustomer method in DataSource class is done

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -20,8 +22,29 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String username) {
+    String query = "SELECT * FROM Customers WHERE USERNAME = ?";
+    Customer customer = null;
+    try (Connection connection = connect(); PreparedStatement statement = connection.prepareStatement(query)) {
+
+      statement.setString(1, username);
+      ResultSet resultSet = statement.executeQuery();
+      customer = new Customer(
+          resultSet.getInt("ID"),
+          resultSet.getString("NAME"),
+          resultSet.getString("USERNAME"),
+          resultSet.getString("PASSWORD"),
+          resultSet.getInt("ACCOUNT_ID"));
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args) {
-    connect();
+    Customer customer = getCustomer("lfromonte9@de.vu");
+    System.out.println(customer.getName());
   }
 
 }
