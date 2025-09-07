3886fdd14fb6a19410adc7eec1bb7c6c64fdd9ab
Seifelden Atia <68141338+SeifeldenAtia@users.noreply.github.com>
2025-09-07 15:01:29 +0000

    Add console menu options for deposit, withdraw, balance check, and exit

---
+++ src/main/java/bank/Menu.java
@@ -0,0 +1,82 @@
+package bank;
+
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+public class Menu {
+
+  private Scanner scanner;
+
+  public static void main(String[] args) {
+    Menu menu = new Menu();
+    menu.scanner = new Scanner(System.in);
+    System.out.println("Welcome to BankName bank!");
+    Customer customer = menu.AuthenticateUser();
+
+    if (customer != null) {
+      Account account = DataSource.geAccount(customer.getAccountId());
+      menu.showMenu(customer, account);
+    }
+
+    menu.scanner.close();
+
+  }
+
+  private Customer AuthenticateUser() {
+    System.out.println("Please enter your username");
+    String username = scanner.next();
+    System.out.println("Please enter your password");
+    String password = scanner.next();
+
+    Customer customer = null;
+    try {
+      customer = Authenticator.login(username, password);
+    } catch (LoginException e) {
+      System.out.println("An Error occurred: " + e.getMessage());
+    }
+
+    return customer;
+  }
+
+  private void showMenu(Customer customer, Account account) {
+    int selection = 0;
+    while (selection != 4 && customer.isAuthenticated()) {
+      System.out.println("=================================================");
+      System.out.println("please select one of the following operations: ");
+      System.out.println("1: Deposit");
+      System.out.println("2: Withdraw");
+      System.out.println("3: Check Balance");
+      System.out.println("4: Exit");
+      System.out.println("=================================================");
+
+      selection = scanner.nextInt();
+      double amount = 0;
+      switch (selection) {
+        case 1:
+          System.out.println("How much would you to deposit? ");
+          amount = scanner.nextDouble();
+          account.deposit(amount);
+          break;
+        case 2:
+          System.out.println("How much would you to withdraw? ");
+          amount = scanner.nextDouble();
+          account.withdraw(amount);
+          break;
+        case 3:
+          System.out.println("Current Balance: " + account.getBalance());
+          break;
+        case 4:
+          Authenticator.logout(customer);
+          System.out.println("Thank you for banking at BankName bank!");
+          break;
+
+        default:
+          System.out.println("Invalid operation, please try again!");
+          break;
+      }
+    }
+
+  }
+
+}
